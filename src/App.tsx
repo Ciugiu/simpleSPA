dc8f33f84701ce5ec139ce07f35f7d96dc51c7e1
ciugiu <corneliu.ciugurean@epita.fr>
2025-04-20 16:39:44 +0200

    feat: NavBar, Pages & Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import NotFoundPage from "./pages/NotFoundPage";
+import CreateProduct from "./pages/CreateProduct";
+
+// Components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center vh-100 w-100">
-      <h1 className="text-center">App</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/create-product" element={<CreateProduct />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 };
 
